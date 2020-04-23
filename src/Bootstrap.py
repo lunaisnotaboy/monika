851a4a221d8d9864d5c4fddb7fb684118af37942
lunaisnotaboy <stampylongr7@gmail.com>
2020-04-22 21:27:09 -0400

    chnage a lot of stuff

---
+++ src/Bootstrap.py
@@ -0,0 +1,15 @@
+# This file is a part of Monika.
+# This is subject to change.
+
+# Import modules.
+import os
+import sys
+from datetime import datetime
+
+def main():
+    # Set variables.
+    love = "everlasting" # <3 love you moni
+    # date & time in terminal
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print("date and time =", dt_string)
--- src/Bootstrap_default.py
+++
@@ -1,9 +0,0 @@
-# This file is a part of Monika.
-# This is subject to change.
-
-# Import modules.
-import os
-import sys
-
-# Set variables.
-modulespath = "yourpathhere"
